253ad64c7fb32318fadea4981c07efbaba1d075a
Basanta Nembang <nembangbasanta66@gmail.com>
2025-04-15 14:48:11 +0545

    fix mistake update

--- M.java
+++ M.java
@@ -4,6 +4,6 @@ public class M {
         System.out.println("SWE Basanta Nembang");
         System.out.println("Java full stack and a dev ops Er");
         System.out.println("Work at USA based enterpries company :: (remote)");
-    }
+    
     }
 }
