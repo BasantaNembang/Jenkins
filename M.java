3d14fe8c30da49c89bafa4d00125db6d4a174c4f
Basanta Nembang <nembangbasanta66@gmail.com>
2025-04-15 14:14:51 +0545

    second update

--- M.java
+++ M.java
@@ -2,5 +2,6 @@ public class M {
 
     public static void main(String[] args) {
         System.out.println("SWE Basanta Nembang");
+        System.out.println("Java full stack and a dev ops Er");
     }
 }
